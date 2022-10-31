5709fd96cfa17b02f2e587a040cb5ed2bfd6eef9
Adrián Fenollar Navarro <adrianfenollarnavarro@gmail.com>
2022-10-31 12:02:36 +0100

    Changes for naming convention

--- src/Matrix.hpp
+++ src/Matrix.hpp
@@ -97,10 +97,10 @@ class Matrix {
    * Resizes the matrix to match m and copies its contents.
    */
   constexpr Matrix& operator=(const Matrix& m) {
-    _rows = m._rows;
-    _columns = m._columns;
+    rows_ = m.rows_;
+    columns_ = m.columns_;
     // TODO: review this
-    _data = m._data;
+    data_ = m.data_;
   }
 
   /**
@@ -110,10 +110,10 @@ class Matrix {
    * The element m is left in an unspecified but valid state.
    */
   constexpr Matrix& operator=(Matrix&& m) noexcept {
-    _rows = m._rows;
-    _columns = m._columns;
+    rows_ = m.rows_;
+    columns_ = m.columns_;
     // TODO: review this
-    _data = std::move(m._data);
+    data_ = std::move(m.data_);
   }
 
   /**
@@ -125,17 +125,17 @@ class Matrix {
    */
   constexpr Matrix& operator=(
       std::initializer_list<std::initializer_list<value_type>> ill) {
-    _rows = ill.size();
-    _columns = 0;
+    rows_ = ill.size();
+    columns_ = 0;
     for (auto& row : ill) {
-      _columns = std::max(_columns, row.size());
+      columns_ = std::max(columns_, row.size());
     }
-    _data.resize(_rows * _columns);
+    data_.resize(rows_ * columns_);
     size_type i = 0;
     for (auto& row : ill) {
       size_type j = 0;
       for (auto& col : row) {
-        _data[i * _columns + j] = col;
+        data_[i * columns_ + j] = col;
         j++;
       }
       i++;
@@ -148,32 +148,32 @@ class Matrix {
   constexpr void assign(size_type n, size_type m) {
     // TODO: check, this only changes the val for new elements when increasing
     // size
-    _data.resize(n * m);
+    data_.resize(n * m);
   }
 
   /**
    * Resizes the matrix and initializes its elements with a copy of val.
    */
   constexpr void assign(size_type n, size_type m, const value_type& val) {
-    _rows = n;
-    _columns = m;
+    rows_ = n;
+    columns_ = m;
     // TODO: check, this only changes the val for new elements when increasing
     // size
-    _data.resize(n * m, val);
+    data_.resize(n * m, val);
   }
 
   /**
    * Returns a reference to the ith row.
    */
   constexpr row_reference operator[](size_type i) noexcept {
-    return &_data[i * _columns];
+    return &data_[i * columns_];
   }
 
   /**
    * Returns a const reference to the ith row.
    */
   constexpr const_row_reference operator[](size_type i) const noexcept {
-    return &_data[i * _columns];
+    return &data_[i * columns_];
   }
 
   /**
@@ -183,7 +183,7 @@ class Matrix {
    * [data(), data() + size()) or [data(), data() + rows()*columns())
    * is valid.
    */
-  constexpr value_type* data() noexcept { return _data.data(); }
+  constexpr value_type* data() noexcept { return data_.data(); }
 
   /**
    * Returns a const pointer to the underlying array serving as element storage.
@@ -192,32 +192,32 @@ class Matrix {
    * [data(), data() + size()) or [data(), data() + rows()*columns())
    * is valid.
    */
-  constexpr value_type* data() const noexcept { return _data.data(); }
+  constexpr value_type* data() const noexcept { return data_.data(); }
 
   /**
    * Checks if the Matrix has no elements.
    */
-  [[nodiscard]] constexpr bool empty() const noexcept { return _rows == 0; }
+  [[nodiscard]] constexpr bool empty() const noexcept { return rows_ == 0; }
 
   /**
    * Returns the total number of elements of the matrix.
    */
-  constexpr size_type size() const noexcept { return _rows * _columns; }
+  constexpr size_type size() const noexcept { return rows_ * columns_; }
 
   /**
    * Returns the number of rows of the matrix.
    */
-  constexpr size_type rows() const noexcept { return _rows; }
+  constexpr size_type rows() const noexcept { return rows_; }
 
   /**
    * Returns the number of columns of the matrix.
    */
-  constexpr size_type columns() const noexcept { return _columns; }
+  constexpr size_type columns() const noexcept { return columns_; }
 
  private:
-  size_type _rows;
-  size_type _columns;
-  std::vector<T> _data;
+  size_type rows_;
+  size_type columns_;
+  std::vector<T> data_;
 };
 
 // TODO(edsa): Write constructors, destructor, assignment operators
